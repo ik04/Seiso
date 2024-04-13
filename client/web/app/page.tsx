9094cc65baab7674a71fbbbbc6eaf3af2e5c0a7c
ishaan khurana <ishaankhurana123@gmail.com>
2024-04-13 16:55:25 +0530

    feat: init auth sheet and finish login component

--- client/web/app/page.tsx
+++ client/web/app/page.tsx
@@ -1,7 +1,12 @@
+import { Landing } from "@/components/pages/landing/landing";
 import React from "react";
 
 const page = () => {
-  return <div className="font-spaceGrotesk text-2xl">lets get this bread</div>;
+  return (
+    <div className="">
+      <Landing />
+    </div>
+  );
 };
 
 export default page;
---
+++ client/web/components/pages/landing/landing.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { AuthSheet } from "./authSheet";
+
+export const Landing = () => {
+  return (
+    <div>
+      <AuthSheet />
+    </div>
+  );
+};
