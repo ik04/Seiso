0855a6ff224fff186b62d9c7b39bdcda80a812fc
ishaan khurana <ishaankhurana123@gmail.com>
2024-05-10 23:44:17 +0530

    feat: add skeletons for loading

--- client/web/components/pages/dashboard/dashboard.tsx
+++ client/web/components/pages/dashboard/dashboard.tsx
@@ -5,10 +5,12 @@ import Link from "next/link";
 import axios from "axios";
 import { GlobalContext } from "@/app/context/GlobalContext";
 import { Slip } from "@/types/Slip";
+import { Skeleton } from "@/components/ui/skeleton";
 
 export const Dashboard = () => {
   const { token } = useContext(GlobalContext);
   const [slips, setSlips] = useState<Slip[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
 
   const fetchSlips = async () => {
     if (token) {
@@ -22,6 +24,7 @@ export const Dashboard = () => {
           }
         );
         setSlips(resp.data.slips);
+        setLoading(false);
       } catch (error) {
         console.error("Error fetching slips:", error);
       }
@@ -40,25 +43,37 @@ export const Dashboard = () => {
           Slips
         </h2>
         <div className="grid grid-cols-5 gap-y-10">
-          {slips.map((slip) => (
-            <div
-              key={slip.uuid}
-              className="bg-azureOcean md:w-80 md:h-[30rem] text-breezyAqua font-spaceGrotesk capitalize font-semibold md:text-2xl text-start flex flex-col justify-center px-3"
-            >
-              {Object.entries(slip.items).map(([key, value]: [string, any]) => (
-                <div className="flex space-x-3" key={key}>
-                  <span>{key}: </span>
-                  <span>{value}</span>
+          {!loading ? (
+            <>
+              {slips.map((slip) => (
+                <div
+                  key={slip.uuid}
+                  className="bg-azureOcean md:w-80 md:h-[30rem] text-breezyAqua font-spaceGrotesk capitalize font-semibold md:text-2xl text-start flex flex-col justify-center px-3"
+                >
+                  {Object.entries(slip.items).map(
+                    ([key, value]: [string, any]) => (
+                      <div className="flex space-x-3" key={key}>
+                        <span>{key}: </span>
+                        <span>{value}</span>
+                      </div>
+                    )
+                  )}
+                  <div className="text-center">{slip.laundry.name}</div>
                 </div>
               ))}
-              <div className="text-center">{slip.laundry.name}</div>
-            </div>
-          ))}
-          <Link href={"/slip/add"}>
-            <div className="bg-breezyAqua w-80 h-[30rem] flex justify-center items-center text-9xl font-spaceGrotesk text-sereneSky">
-              +
-            </div>
-          </Link>
+              <Link href={"/slip/add"}>
+                <div className="bg-breezyAqua w-80 h-[30rem] flex justify-center items-center text-9xl font-spaceGrotesk text-sereneSky">
+                  +
+                </div>
+              </Link>
+            </>
+          ) : (
+            <>
+              {Array.from({ length: 10 }, (_, index) => (
+                <Skeleton className="bg-sereneSky md:w-80 md:h-[30rem]" />
+              ))}
+            </>
+          )}
         </div>
       </div>
     </div>
