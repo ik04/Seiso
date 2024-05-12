961b48044a3113f145c582997c7cad2660b1bd6f
ishaan khurana <ishaankhurana123@gmail.com>
2024-05-13 02:58:10 +0530

    feat: finish card frontend

--- client/web/components/pages/dashboard/dashboard.tsx
+++ client/web/components/pages/dashboard/dashboard.tsx
@@ -51,7 +51,7 @@ export const Dashboard = () => {
                   key={slip.uuid}
                   className="bg-azureOcean md:w-80 md:h-[30rem] text-creamyPeach font-spaceGrotesk capitalize font-semibold md:text-2xl text-start flex flex-col justify-between py-5 px-3"
                 >
-                  <div className="border-b-2 border-dashed border-creamyPeach text-center p-2 text-3xl">
+                  <div className="border-b-2 border-dashed border-creamyPeach text-center p-3 text-3xl">
                     {slip.laundry.name}
                   </div>
                   <div className="flex-1 flex flex-col justify-center space-y-5">
@@ -64,7 +64,7 @@ export const Dashboard = () => {
                       )
                     )}
                   </div>
-                  <div className="flex-2 border-t-2 border-dashed border-creamyPeach">
+                  <div className="flex-2 border-t-2 border-dashed border-creamyPeach flex items-start justify-between pt-3">
                     <div className="stats text-breezyAqua text-start p-2">
                       <div className="uppercase">Total: {slip.total_items}</div>
                       <div className="text-base">
@@ -74,9 +74,12 @@ export const Dashboard = () => {
                         </div>
                       </div>
                     </div>
-                    <div className="buttons">
-                      <div className="view capitalize bg-breezyAqua text-azureOcean">
+                    <div className="buttons text-base text-center flex flex-col space-y-3 justify-center p-3 h-full">
+                      <div className="view capitalize bg-breezyAqua text-azureOcean p-[0.2rem] rounded-lg">
                         view
+                      </div>
+                      <div className="view capitalize bg-breezyAqua text-azureOcean p-[0.2rem] rounded-lg">
+                        Process
                       </div>
                       <div className=""></div>
                     </div>
