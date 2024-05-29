7496fcb2de0e190a177341e5afa9582213becf06
ishaan khurana <ishaankhurana123@gmail.com>
2024-05-29 20:16:02 +0530

    feat: add vercel.json in rest folder

--- client/web/components/pages/landing/landing.tsx
+++ client/web/components/pages/landing/landing.tsx
@@ -1,5 +1,10 @@
 import React, { useEffect } from "react";
+import animationData from "../../../app/lottie/laundryDude.json";
 
 export const Landing = () => {
+  const options = {
+    animationData: animationData,
+    loop: true,
+  };
   return <div className=""></div>;
 };
